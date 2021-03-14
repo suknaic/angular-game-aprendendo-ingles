9ce00ef2761251add44f47440c25b438aae39d92
Felipe Suknaic <fsuknaic@hotmail.com>
2021-03-14 02:03:58 -0500

    mock do aplicativo

---
+++ src/app/mock/frases.mock.ts
@@ -0,0 +1,12 @@
+import { Frase } from '../model/frase.model';
+
+const Frases: Frase[] = [
+  { frasePortugues: 'Eu gosto de Aprender', fraseIngles: 'I like to learn' },
+  { frasePortugues: 'não pare de aprender', fraseIngles: 'not stop learning' },
+  {
+    frasePortugues: 'sejam heróis de sua próprias histórias',
+    fraseIngles: 'be heroes of your own stories',
+  },
+];
+
+export default Frases;
